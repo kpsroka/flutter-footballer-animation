cdd0e65c760b8156100aef597c799620d99d6780
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2019-01-18 07:40:34 +0100

    Refactoring/simplifying Y points calculation

--- lib/stickman.dart
+++ lib/stickman.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class Stickman extends StatefulWidget {
   final AnimationController animationController;
 
-  const Stickman({Key key, this.animationController}) : super(key: key); 
+  const Stickman({Key key, this.animationController}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -34,49 +34,23 @@ class _StickmanState extends State<Stickman>
 }
 
 class _PathCalculator {
-  final List<double> footStagesX;
-  final List<double> kneeStagesX;
-  List<double> footStagesY;
-  List<double> kneeStagesY;
+  final List<Point> footStages;
+  final List<Point> kneeStages;
   final Point hip;
   final double partLength;
-  final List<int> kneeFootDirectionUpIndexes;
+
+  static Point lerpPoint(Point a, Point b, double delta) =>
+      Point(lerp(a.x, b.x, delta), lerp(a.y, b.y, delta));
 
   static double lerp(double a, double b, double delta) =>
       a * (1 - delta) + b * delta;
 
-  _PathCalculator(
-      {@required this.footStagesX,
-      @required this.kneeStagesX,
+  _PathCalculator.fromPoints(
+      {@required this.footStages,
+      @required this.kneeStages,
       @required this.hip,
-      @required this.partLength,
-      @required this.kneeFootDirectionUpIndexes}) {
-    assert(footStagesX.length == kneeStagesX.length);
-    this.kneeStagesY = List.generate(
-        footStagesX.length,
-        (index) => _getTargetY(
-            sourceX: hip.x,
-            sourceY: hip.y,
-            targetX: kneeStagesX[index],
-            upwards: false));
-    this.footStagesY = List.generate(
-        footStagesX.length,
-        (index) => _getTargetY(
-            sourceX: kneeStagesX[index],
-            sourceY: kneeStagesY[index],
-            targetX: footStagesX[index],
-            upwards: kneeFootDirectionUpIndexes.contains(index)));
-  }
-
-  double _getTargetY(
-      {@required double sourceX,
-      @required double sourceY,
-      @required double targetX,
-      @required bool upwards}) {
-    final sqDelta = pow(partLength, 2) - pow(sourceX - targetX, 2);
-    final yDelta = sqDelta > 0.0 ? sqrt(sqDelta) : 0;
-    return upwards ? sourceY - yDelta : sourceY + yDelta;
-  }
+      @required this.partLength})
+      : assert(footStages.length == kneeStages.length);
 
   List<Point> getPointsForProgress(double progress) {
     assert(progress >= 0.0);
@@ -86,128 +60,119 @@ class _PathCalculator {
       return getPointsForProgress(0.0);
     }
 
-    int stage = (footStagesX.length * progress).floor();
-    int mirror = (stage + footStagesX.length ~/ 2) % footStagesX.length;
-    double stageProgress = (footStagesX.length * progress - stage);
+    int stage = (footStages.length * progress).floor();
+    int stageNext = (stage + 1) % footStages.length;
+    int mirror = (stage + footStages.length ~/ 2) % footStages.length;
+    int mirrorNext = (mirror + 1) % footStages.length;
+    double stageProgress = (footStages.length * progress - stage);
 
-    double stageFootX = lerp(footStagesX[stage],
-        footStagesX[(stage + 1) % footStagesX.length], stageProgress);
-    double stageKneeX = lerp(kneeStagesX[stage],
-        kneeStagesX[(stage + 1) % footStagesX.length], stageProgress);
-    double stageFootY = lerp(footStagesY[stage],
-        footStagesY[(stage + 1) % footStagesX.length], stageProgress);
-    double stageKneeY = lerp(kneeStagesY[stage],
-        kneeStagesY[(stage + 1) % footStagesX.length], stageProgress);
-    double mirrorFootX = lerp(footStagesX[mirror],
-        footStagesX[(mirror + 1) % footStagesX.length], stageProgress);
-    double mirrorKneeX = lerp(kneeStagesX[mirror],
-        kneeStagesX[(mirror + 1) % footStagesX.length], stageProgress);
-    double mirrorFootY = lerp(footStagesY[mirror],
-        footStagesY[(mirror + 1) % footStagesX.length], stageProgress);
-    double mirrorKneeY = lerp(kneeStagesY[mirror],
-        kneeStagesY[(mirror + 1) % footStagesX.length], stageProgress);
-
+    Point stageFootPoint =
+        lerpPoint(footStages[stage], footStages[stageNext], stageProgress);
+    Point stageKneePoint =
+        lerpPoint(kneeStages[stage], kneeStages[stageNext], stageProgress);
+    Point mirrorFootPoint =
+        lerpPoint(footStages[mirror], footStages[mirrorNext], stageProgress);
+    Point mirrorKneePoint =
+        lerpPoint(kneeStages[mirror], kneeStages[mirrorNext], stageProgress);
     return [
-      Point(stageFootX, stageFootY),
-      Point(stageKneeX, stageKneeY),
+      stageFootPoint,
+      stageKneePoint,
       hip,
-      Point(mirrorKneeX, mirrorKneeY),
-      Point(mirrorFootX, mirrorFootY)
+      mirrorKneePoint,
+      mirrorFootPoint,
     ];
   }
 }
 
 class StickmanPainter extends CustomPainter {
-  static _PathCalculator legCalculator = _PathCalculator(
-    footStagesX: [
-      .243,
-      .314,
-      .357,
-      .400,
-      .471,
-      .557,
-      .621,
-      .700,
-      .769,
-      .764,
-      .750,
-      .707,
-      .536,
-      .443,
-      .343,
-      .243,
-      .193,
-      .200,
+  static _PathCalculator legPointCalculator = _PathCalculator.fromPoints(
+    footStages: [
+      Point(0.243, 0.678),
+      Point(0.314, 0.638),
+      Point(0.357, 0.637),
+      Point(0.4, 0.656),
+      Point(0.471, 0.754),
+      Point(0.557, 0.805),
+      Point(0.621, 0.851),
+      Point(0.7, 0.857),
+      Point(0.769, 0.857),
+      Point(0.764, 0.915),
+      Point(0.75, 0.925),
+      Point(0.707, 0.956),
+      Point(0.536, 1.0),
+      Point(0.443, 0.989),
+      Point(0.343, 0.961),
+      Point(0.243, 0.908),
+      Point(0.193, 0.873),
+      Point(0.2, 0.804),
     ],
-    kneeStagesX: [
-      .471,
-      .521,
-      .567,
-      .629,
-      .700,
-      .728,
-      .728,
-      .736,
-      .736,
-      .693,
-      .686,
-      .650,
-      .557,
-      .529,
-      .479,
-      .421,
-      .385,
-      .436,
+    kneeStages: [
+      Point(0.471, 0.759),
+      Point(0.521, 0.763),
+      Point(0.567, 0.757),
+      Point(0.629, 0.734),
+      Point(0.7, 0.676),
+      Point(0.728, 0.634),
+      Point(0.728, 0.634),
+      Point(0.736, 0.617),
+      Point(0.736, 0.617),
+      Point(0.693, 0.684),
+      Point(0.686, 0.691),
+      Point(0.65, 0.721),
+      Point(0.557, 0.759),
+      Point(0.529, 0.763),
+      Point(0.479, 0.76),
+      Point(0.421, 0.744),
+      Point(0.385, 0.726),
+      Point(0.436, 0.75),
     ],
-    kneeFootDirectionUpIndexes: [0, 1, 2, 3],
     hip: Point(.514, .521),
     partLength: .242,
   );
 
-  static _PathCalculator armCalculator = _PathCalculator(
-    footStagesX: [
-      .457,
-      .521,
-      .622,
-      .715,
-      .736,
-      .757,
-      .721,
-      .700,
-      .686,
-      .757,
-      .700,
-      .657,
-      .493,
-      .385,
-      .372,
-      .348,
-      .364,
-      .400,
+  static _PathCalculator armPointCalculator = _PathCalculator.fromPoints(
+    footStages: [
+      Point(0.457, 0.583),
+      Point(0.521, 0.622),
+      Point(0.622, 0.612),
+      Point(0.715, 0.54),
+      Point(0.736, 0.329),
+      Point(0.757, 0.251),
+      Point(0.721, 0.214),
+      Point(0.7, 0.183),
+      Point(0.686, 0.168),
+      Point(0.757, 0.325),
+      Point(0.7, 0.515),
+      Point(0.657, 0.554),
+      Point(0.493, 0.612),
+      Point(0.385, 0.564),
+      Point(0.372, 0.549),
+      Point(0.348, 0.517),
+      Point(0.364, 0.53),
+      Point(0.4, 0.55),
     ],
-    kneeStagesX: [
-      .429,
-      .514,
-      .550,
-      .550,
-      .557,
-      .629,
-      .643,
-      .671,
-      .686,
-      .579,
-      .521,
-      .507,
-      .479,
-      .407,
-      .393,
-      .372,
-      .379,
-      .393,
+    kneeStages: [
+      Point(0.429, 0.383),
+      Point(0.514, 0.42),
+      Point(0.55, 0.423),
+      Point(0.55, 0.423),
+      Point(0.557, 0.423),
+      Point(0.629, 0.407),
+      Point(0.643, 0.4),
+      Point(0.671, 0.383),
+      Point(0.686, 0.37),
+      Point(0.579, 0.421),
+      Point(0.521, 0.421),
+      Point(0.507, 0.418),
+      Point(0.479, 0.41),
+      Point(0.407, 0.364),
+      Point(0.393, 0.348),
+      Point(0.372, 0.316),
+      Point(0.379, 0.329),
+      Point(0.393, 0.348),
     ],
     hip: Point(.550, .221),
     partLength: .202,
-    kneeFootDirectionUpIndexes: [4, 5, 6, 7, 8, 9],
   );
 
   final Animation<double> aController;
@@ -243,8 +208,8 @@ class StickmanPainter extends CustomPainter {
       ..strokeJoin = StrokeJoin.round
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 0.093 * s;
-    List<Point> legPoints = legCalculator.getPointsForProgress(progress);
-    List<Point> armPoints = armCalculator.getPointsForProgress(progress);
+    List<Point> legPoints = legPointCalculator.getPointsForProgress(progress);
+    List<Point> armPoints = armPointCalculator.getPointsForProgress(progress);
 
     canvas
       ..drawPath(_pointsToPath(legPoints, s), _paint)
